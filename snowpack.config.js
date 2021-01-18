ae747b86eb2051b2f4e7705990c86904ce95690b
Paul Armstrong <paul@spaceyak.com>
2021-01-18 10:52:25 -0800

    chore: update snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,25 +5,16 @@ module.exports = {
     src: '/_dist_',
   },
   plugins: ['@snowpack/plugin-dotenv', '@prefresh/snowpack'],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
-    sourceMaps: true,
-    /* ... */
+  optimize: {
+    bundle: true,
+    minify: true,
+    treeshake: true,
+    target: 'es2019',
   },
-  devOptions: {
-    /* ... */
+  packageOptions: {
+    sourcemap: true,
   },
   buildOptions: {
-    sourceMaps: true,
-    /* ... */
-  },
-  proxy: {
-    /* ... */
-  },
-
-  alias: {
-    /* ... */
+    sourcemap: true,
   },
 };
